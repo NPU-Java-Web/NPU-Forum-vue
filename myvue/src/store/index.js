598a0eb282e51531e3739a1c99c0e863f20fbf94
MCL <1693738649@qq.com>
2021-09-30 20:48:06 +0800

    合并修改

--- myvue/src/store/index.js
+++ myvue/src/store/index.js
@@ -6,13 +6,19 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   //存放静态变量
   state:{
-    localid:''
+    localid:'',
+    localNickname:''
   },
   //输入静态变量的方法
   mutations:{
-    saveLocalid(state,localid){
-      this.state.localid=localid;
-    }
+    saveLocalid(state,localid) {
+      this.state.localid = localid;
+    },
+      saveNickname(state,localNickname)
+      {
+        this.state.localNickname=localNickname;
+      }
+
   }
 
 })
